7bdef4ce2a3322237b8f603be8a3f6cc7a20277d
LAPTOP-EL7JIBU8\Lenovo <laingocbui@gmail.com>
2024-09-11 22:37:09 +0700

    add cosmos serive

---
+++ PhotoProcessor/PhotoProcessor/Data/CosmosDbService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos;
+using PhotoProcessor.Models;
+
+namespace PhotoProcessor.Data
+{
+    public class CosmosDbService
+    {
+        private readonly Container _container;
+
+        public CosmosDbService(CosmosClient cosmosClient, string databaseName, string containerName)
+        {
+            _container = cosmosClient.GetContainer(databaseName, containerName);
+        }
+
+        public async Task AddPhotoMetadataAsync(PhotoMetadata metadata)
+        {
+            await _container.CreateItemAsync(metadata, new PartitionKey(metadata.id.ToString()));
+        }
+    }
+}
